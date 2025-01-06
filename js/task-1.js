4155ec66019d6132c0f884c26a6c40035b818699
Oleksandr Nechaiev <snov1234@gmail.com>
2025-01-06 14:14:33 +0200

    finish_dz4

--- js/task-1.js
+++ js/task-1.js
@@ -1,20 +1,24 @@
-function slugify(title) {
-    
-  const lowerCaseTitle = title.toLowerCase();
-  
-  const words = lowerCaseTitle.split(" ");
-  
-  const slug = words.join("-");
-  
-  return slug;
+function isEnoughCapacity(products, containerSize) {
+  const wares = Object.values(products);
+  let totalItems = 0;
+  for (const ware of wares) {
+    totalItems += ware;
+  }
+    return totalItems <= containerSize;
 }
 
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
 
-console.log(slugify("Arrays for beginners")); 
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
 
-console.log(slugify("English for developer")); 
-
-console.log(slugify("Ten secrets of JavaScript")); 
-
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
